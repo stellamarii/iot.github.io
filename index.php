ec54bfde63f3e0fa39fe6e71937ff2853ee5b88d
smtamme <stellamariitamme@gmail.com>
2022-09-09 08:43:49 +0300

    html

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    $myfile = fopen("text.txt", "w") or die("Unable to open file!");
+    $txt = "John Doe\n";
+    fwrite($myfile, $txt);
+    $txt = "Jane Doe\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>Tere</div>
+    <input type="button" value="yes" name="yes"/>
+    <input type="button" value="no" name="no"/>
+</body>
+</html>
